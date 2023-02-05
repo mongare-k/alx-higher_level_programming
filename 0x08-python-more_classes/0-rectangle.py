7dbe33ac1af786b8fb8e5dd9e9e7521e9692e7e8
Kevin Mong'are <molessk@gmail.com>
2023-02-05 04:43:44 +0000

    First commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on 05/02/2023
+@author: Kevin Mong'are
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
+
+    pass
